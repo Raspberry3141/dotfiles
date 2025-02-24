2bec7c1c8431e020ff5820c2631f933cb7d65754
laptop admin <masa.namba101@gmail.com>
2025-02-23 19:22:48 -0800

    a

---
+++ .dotfiles/.bashrc
@@ -0,0 +1,12 @@
+#
+# ~/.bashrc
+#
+function cl() {
+	cd "$@" && ls;
+}
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
---
+++ load_dotfiles.sh
@@ -0,0 +1,17 @@
+#!/usr/biin/bash
+
+function load {
+	#1. make the directory necessary
+	#2. cp files to the location and rename
+	echo $1;
+	mkdir --parents "$1";
+	cp "$2" "$1/$3";
+}
+
+load "/home/admin" "/home/admin/.dotfiles/.bash_profile" ".bash_profile"
+load "/home/admin" "/home/admin/.dotfiles/.bashrc" ".bashrc"
+load "/home/admin" "/home/admin/.dotfiles/.xinit" ".xinit"
+load "/home/admin" "/home/admin/.dotfiles/.Xresources" ".Xresources"
+load "/home/admin/.config/i3" "/home/admin/.dotfiles/i3" "config"
+load "/home/admin/.config/i3status" "/home/admin/.dotfiles/i3status" "config"
+load "/home/admin/.config/nvim" "/home/admin/.dotfiles/init.lua" "init.lua"
