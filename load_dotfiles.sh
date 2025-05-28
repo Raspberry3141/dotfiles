1b758bc15fb0133e25561eacabd82515dfa99e82
laptop admin <masa.namba101@gmail.com>
2025-05-28 13:08:56 -0700

    load

--- load_dotfiles.sh
+++ load_dotfiles.sh
@@ -3,16 +3,15 @@
 function load {
 	#1. make the directory necessary
 	#2. cp files to the location and rename
-	echo $1;
 	mkdir --parents "$1";
 	cp "$2" "$1/$3";
 }
 
-load "/home/admin" "/home/admin/.dotfiles/.bash_profile" ".bash_profile"
-load "/home/admin" "/home/admin/.dotfiles/.bashrc" ".bashrc"
-load "/home/admin" "/home/admin/.dotfiles/.xinit" ".xinit"
-load "/home/admin" "/home/admin/.dotfiles/.Xresources" ".Xresources"
-load "/home/admin/.config/i3" "/home/admin/.dotfiles/i3" "config"
-load "/home/admin/.config/i3status" "/home/admin/.dotfiles/i3status" "config"
-load "/home/admin/.config/nvim" "/home/admin/.dotfiles/init.lua" "init.lua"
-load "/home/admin/.config/alacritty" "/home/admin/.dotfiles/alacritty" "alacritty.toml"
+load "$HOME" "$HOME/.dotfiles/.bash_profile" ".bash_profile"
+load "$HOME" "$HOME/.dotfiles/.bashrc" ".bashrc"
+load "$HOME" "$HOME/.dotfiles/.xinit" ".xinit"
+load "$HOME" "$HOME/.dotfiles/.Xresources" ".Xresources"
+load "$HOME" "$HOME/.dotfiles/i3" "config"
+load "$HOME" "$HOME/.dotfiles/i3status" "config"
+load "$HOME" "$HOME/.dotfiles/init.lua" "init.lua"
+load "$HOME" "$HOME/.dotfiles/alacritty" "alacritty.toml"
